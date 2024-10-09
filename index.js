4016a3768b2f8c9411d9ed549521d10b48672b36
gzliew <guozhangliew@gmail.com>
2024-10-09 10:53:45 +0800

    update node server to handle request

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
 // content of index.js
 const http = require("http");
 const fs = require("fs");
-const port = 8080;
+const port = 3000;
 
-const requestHandler = (request, response) => {
-    console.log(request.url);
-    const html = fs.readFileSync("./index.html");
-    response.writeHeader(200, { "Content-Type": "text/html" });
-    response.write(html);
-    response.end();
+const handleIncomingPostRequest = async (urlPath, body) => {
+  console.log("body", JSON.stringify(body, null, 2));
+  return { success: true };
 };
 
-const server = http.createServer(requestHandler);
+const server = http.createServer((request, response) => {
+  console.log(request.url);
 
-server.listen(port, err => {
-    if (err) {
-        console.log("something bad happened", err);
-        throw err;
-    }
+  switch (request.method.toUpperCase()) {
+    case "POST":
+      var reqBody = "";
 
-    console.log(`server is listening on ${port}`);
+      request.on("data", function (chunk) {
+        reqBody += chunk;
+      });
+
+      request.on("end", async function () {
+        const body = JSON.parse(reqBody);
+        const data = await handleIncomingPostRequest(request.url, body);
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.write(JSON.stringify(data));
+        response.end();
+        return;
+      });
+      return;
+    case "GET":
+      const html = fs.readFileSync("./index.html");
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(html);
+      response.end();
+    default:
+      break;
+  }
+});
+
+server.listen(port, (err) => {
+  if (err) {
+    console.log("something bad happened", err);
+    throw err;
+  }
+  console.log(`server is listening on ${port}`);
 });

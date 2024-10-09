c1711859211057cedb2a261e2ae3b8dc751c05f9
gzliew <guozhangliew@gmail.com>
2024-10-09 11:09:47 +0800

    update server get post req

--- index.js
+++ index.js
@@ -3,9 +3,75 @@ const http = require("http");
 const fs = require("fs");
 const port = 3000;
 
-const handleIncomingPostRequest = async (urlPath, body) => {
-  console.log("body", JSON.stringify(body, null, 2));
+const handlePostRequest = async (urlPath, body) => {
+  console.log("{body,urlPath}", JSON.stringify({ body, urlPath }, null, 2));
+
+  if (urlPath === "/v1/tse/device/000A8000FC0000001/tkorouter/thermostat") {
+    return { success: true };
+  }
+
+  if (urlPath === "/auth/token" && "username" in body && "password" in body) {
+    return {
+      accessToken: "{access_token}",
+      scope: "all",
+      expiresIn: 86400,
+      tokenType: "Bearer",
+    };
+  }
+
+  if (urlPath === "/devices/abcd1234/commands" && "attribute" in body) {
+    return { success: true };
+  }
+
   return { success: true };
+};
+
+const handleGetRequest = (urlPath) => {
+  if (urlPath === "/devices/abcd1234") {
+    return {
+      success: true,
+      message: "",
+      result: {
+        name: "Bedroom ceiling light",
+        id: "124",
+        type: "DimmerSwitch",
+        attributes: [
+          {
+            attribute: "status",
+            value: true,
+          },
+          {
+            attribute: "dimLevel",
+            value: 100,
+          },
+        ],
+      },
+    };
+  }
+
+  if (urlPath === "/v1/tse/device/000A8000FC0000001/tkorouter/thermostat") {
+    return {
+      "fanspeed-allowed": ["low", "medium", "high", "auto"],
+      "heat-setpoint-min": 18.88,
+      temperature: 21.66,
+      "fanspeed-state": "medium",
+      "mode-state": "heat",
+      "cool-setpoint": 22.22,
+      "heat-setpoint": 22.22,
+      "mode-allowed": ["cool", "off"],
+      "cool-setpoint-min": 18.88,
+      "heat-setpoint-max": 26.66,
+      address: "000A8000FC000001",
+      fanspeed: "medium",
+      "cool-setpoint-max": 26.66,
+      _link: {
+        _self: "/v1/tse/device/000A8000FC000001/tkorouter/thermostat",
+      },
+      occupied: true,
+      mode: "auto",
+    };
+  }
+  return null;
 };
 
 const server = http.createServer((request, response) => {
@@ -21,7 +87,7 @@ const server = http.createServer((request, response) => {
 
       request.on("end", async function () {
         const body = JSON.parse(reqBody);
-        const data = await handleIncomingPostRequest(request.url, body);
+        const data = await handlePostRequest(request.url, body);
         response.writeHead(200, { "Content-Type": "application/json" });
         response.write(JSON.stringify(data));
         response.end();
@@ -29,9 +95,11 @@ const server = http.createServer((request, response) => {
       });
       return;
     case "GET":
+      const data = handleGetRequest(request.url);
       const html = fs.readFileSync("./index.html");
+      const responseData = data ? JSON.stringify(data) : html;
       response.writeHead(200, { "Content-Type": "text/html" });
-      response.write(html);
+      response.write(responseData);
       response.end();
     default:
       break;
